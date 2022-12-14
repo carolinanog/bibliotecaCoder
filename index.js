763f5c4cbcc655d961660440f9dab9483802d068
Carolina Nogueira <carolina.tinen@alumni.usp.br>
2022-12-14 20:28:56 -0300

    alter port

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(express.urlencoded({extended : true})) //URL com dados
 app.use(express.static(path.resolve())) // Define uma pasta que disponibilizará os arquivos estáticos
 app.set("view engine", "ejs");
 app.use(route)
-app.listen(3000, ()=>{
-    console.log('Acessar http://localhost:3000/paginainicial')
-    console.log('Servidor executando na porta 3000')
+app.listen(80, ()=>{
+    console.log('Acessar http://localhost:80/paginainicial')
+    console.log('Servidor executando na porta 80')
 })
